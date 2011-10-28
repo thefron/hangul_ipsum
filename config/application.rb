37ccfbe37278e8307b777f0f12639ca96e154219
Hoseong Hwang <thefron@wafflestudio.com>
2011-10-28 10:39:42 +0900

    Add precompile assets

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,6 @@ module HangulIpsum
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.precompile += ['home.css', 'home.js']
   end
 end
